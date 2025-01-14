abc67fcc47aaf20189188ec7e8e9bbea043555e8
Luka Barth <lukadbarth@hotmail.com>
2025-01-14 19:39:24 -0300

    Adiciona página sobre

---
+++ src/app/sobre/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+import React from 'react';
+
+const SobrePage = () => {
+  return (
+    <div>
+      <h1>Página de sobre</h1>
+      <Link href="/">Ir para Home</Link>
+    </div>
+  );
+};
+
+export default SobrePage;
