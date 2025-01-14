c0002f045e324018c69fe891b7b68d388e0d48fa
Luka Barth <lukadbarth@hotmail.com>
2025-01-14 19:41:49 -0300

    Modifica página home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
-import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello World Next.js</h1>
+      <h1>Home</h1>
+      <Link href="/sobre">Sobre</Link>
+
+      <h1>Acessar posts</h1>
+      <Link href="/posts">Posts</Link>
+
+      <Link href="/exemplo?parametro=abc">Página com parâmetro</Link>
+
+      <Link href="/produtos/categorias/roupas">Ir a categoria de Roupas</Link>
     </main>
   );
 }
