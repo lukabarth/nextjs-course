faf43618695c3d4cb4f3091e05f331db9cf249e0
Luka Barth <lukadbarth@hotmail.com>
2025-01-15 17:48:16 -0300

    Cria arquivo de layout

---
+++ src/app/dashboard/layout.js
@@ -0,0 +1,10 @@
+const DashboardLayout = ({ children }) => {
+  return (
+    <div>
+      <nav>Links das dashboard</nav>
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default DashboardLayout
