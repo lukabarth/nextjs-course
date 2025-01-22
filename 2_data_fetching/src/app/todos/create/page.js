6ab4b911f0044274885399b30cacc8b4c0f3f271
Luka Barth <lukadbarth@hotmail.com>
2025-01-22 19:12:28 -0300

    Inserindo dados no banco de dados

--- 2_data_fetching/src/app/todos/create/page.js
+++ 2_data_fetching/src/app/todos/create/page.js
@@ -1,9 +1,30 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
 const TodoPage = () => {
 
   const addTodo = async(formData) => {
     "use server"
 
     console.log(formData);
+
+    const titulo = formData.get("titulo");
+    const descricao = formData.get("descricao");
+    const status = "pendente";
+
+    console.log(titulo, descricao);
+
+    const todo = await db.todo.create({
+      data: {
+        titulo,
+        descricao,
+        status,
+      },
+    });
+
+    console.log(todo);
+
+    redirect("/");
   };
 
   return (
