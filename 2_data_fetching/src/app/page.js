8813a9328dc239ce29860435d5f1fa9ed300efdb
Luka Barth <lukadbarth@hotmail.com>
2025-01-22 19:20:42 -0300

    Consulta dados no banco de dados

--- 2_data_fetching/src/app/page.js
+++ 2_data_fetching/src/app/page.js
@@ -1,9 +1,26 @@
+import { db } from "@/db";
+
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const todos = await db.todo.findMany();
+
   return (
-    <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <main className="container mx-auto p-4">
       <Link href="/todos/create">Ir para criação de todo</Link>
+      <h1 className="text-2xl font-bold mb-4">Todos!</h1>
+      <div className="space-y-4">
+        {todos.map((todo) =>(
+          <div key={todo.id} className="bg-gray-100 rounded-lg shadow">
+            <div className="flex justify-between items-start">
+              <div>
+                <h2>{todo.titulo}</h2>
+                <p>{todo.descricao}</p>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
