87a07e01b76006332c6e3781f3fb8f6d141ff2b3
Luka Barth <lukadbarth@hotmail.com>
2025-01-25 13:49:30 -0300

    hook useActionState para lidar com validações no formulário

--- 2_data_fetching/src/actions.js
+++ 2_data_fetching/src/actions.js
@@ -4,18 +4,16 @@ import { db } from "./db";
 import { redirect } from "next/navigation";
 
 export async function deleteTodo(formData) {
-
   const id = Number(formData.get("id"));
 
   await db.todo.delete({
     where: { id },
-  })
+  });
 
   redirect("/");
 }
 
 export async function addTodo(formData) {
-
   const titulo = formData.get("titulo");
   const descricao = formData.get("descricao");
   const status = "pendente";
@@ -29,12 +27,37 @@ export async function addTodo(formData) {
   });
 
   redirect("/");
-};
+}
 
 export const findTodoById = async (id) => {
   const todo = await db.todo.findFirst({
-    where: {id},
-  })
+    where: { id },
+  });
 
   return todo;
-}
+};
+
+export const updateTodo = async (formState, formData) => {
+  const id = Number(formData.get("id"));
+  const titulo = formData.get("titulo");
+  const descricao = formData.get("descricao");
+
+  if (titulo.length < 5) {
+    return {
+      errors: "O titulo precisa de pelo menos 5 caracteres.",
+    };
+  }
+
+  if (descricao.length < 10) {
+    return {
+      errors: "A descrição precisa de pelo menos 10 caracteres.",
+    };
+  }
+
+  await db.todo.update({
+    where: { id },
+    data: { titulo, descricao },
+  });
+
+  redirect("/");
+};
--- 2_data_fetching/src/app/todos/[id]/edit/page.js
+++ 2_data_fetching/src/app/todos/[id]/edit/page.js
@@ -1,15 +1,19 @@
 import { findTodoById } from "@/actions";
+import TodoForm from "@/components/TodoForm";
 
-const TodoEdit = async ({params}) => {
+const TodoEdit = async ({ params }) => {
   const id = Number(params.id);
 
   const todo = await findTodoById(id);
 
   return (
-    <div>
-      <h1>Editando: {todo.titulo}</h1>
+    <div className="max-w-md mx-auto mt-10">
+      <h1 className="text-2xl font-bold text-center mb-6">
+        Editando: {todo.titulo}
+      </h1>
+      <TodoForm todo={todo} />
     </div>
-  )
-}
+  );
+};
 
 export default TodoEdit;
